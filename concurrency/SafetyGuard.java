5e000cc582034e8367bd4e3a49bf65a98078415d
Aaysha82 <aayska214@gmail.com>
2026-03-15 16:00:50 +0530

    Add semaphore management and safety checks in SafetyGuard
    Implemented semaphore management for aisles and added safety checks for robot access.

--- concurrency/SafetyGuard.java
+++ concurrency/SafetyGuard.java
@@ -1,7 +1,71 @@
 // SafetyGuard.java
 // Semaphores and Banker’s Algorithm
-// [cite: 34, 36]
+
+package concurrency;
+
+import java.util.concurrent.Semaphore;
 
 public class SafetyGuard {
-    // TODO: implement semaphores and banker algorithm
+
+    // Example: warehouse has 5 aisles
+    private Semaphore[] aisles;
+
+    // Track how many robots are inside
+    private int availableAisles;
+
+    public SafetyGuard(int numberOfAisles) {
+
+        aisles = new Semaphore[numberOfAisles];
+
+        for (int i = 0; i < numberOfAisles; i++) {
+            aisles[i] = new Semaphore(1);   // only 1 robot allowed
+        }
+
+        availableAisles = numberOfAisles;
+    }
+
+    // Simple Banker style safety check
+    public boolean isSafe() {
+
+        if (availableAisles > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Robot entering aisle
+    public void enterAisle(int taskID, int aisle) {
+
+        try {
+
+            System.out.println("Task " + taskID + " requesting Aisle " + aisle);
+
+            if (!isSafe()) {
+                System.out.println("System NOT SAFE. Task waiting.");
+                return;
+            }
+
+            aisles[aisle].acquire();
+
+            availableAisles--;
+
+            System.out.println("Aisle " + aisle + " LOCKED by Task " + taskID);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Robot leaving aisle
+    public void leaveAisle(int taskID, int aisle) {
+
+        aisles[aisle].release();
+
+        availableAisles++;
+
+        System.out.println("Task " + taskID + " leaving Aisle " + aisle);
+
+        System.out.println("Aisle " + aisle + " RELEASED");
+    }
 }
